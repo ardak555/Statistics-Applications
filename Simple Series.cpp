b23daade2f983684a249302f151767ce44f64a84
Arda Koyuncu <79996371+ardak555@users.noreply.github.com>
2022-03-01 18:32:16 +0300

    Add files via upload

---
+++ Simple Series.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arrleng, i, temp, k, j;
+    cout << "Please enter the number of numbers you want to enter";
+    cin >> arrleng;
+
+    int *arr= new int[arrleng];
+
+    cout << "Please enter the numbers to be sorted : ";
+    for (i = 0; i < arrleng; i++) {
+       
+        cin >> k;
+        arr[i] = k;
+    }
+	cout << "Chosen number: ";
+	for (i = 0; i < arrleng; i++)
+		cout<< arr[i] << " ";
+        
+
+	
+
+	for (i = 0; i < arrleng; i++)
+	{
+		for (j = 0; j < i; j++) {
+			if (arr[i] < arr[j]) {
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+	cout << "Shorted number:";
+	for (i = 0; i < arrleng; i++)
+		cout << arr[i] << " ";
+		
+
+	
+	return 0;
+
+}
+	
+	
