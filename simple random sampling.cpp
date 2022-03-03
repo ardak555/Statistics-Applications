6ce77bd7e80a662786891cb08aa5752b45cb4633
Arda Koyuncu <79996371+ardak555@users.noreply.github.com>
2022-03-03 18:24:04 +0300

    Add files via upload

---
+++ simple random sampling.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <cstdlib> 
+#include <ctime> 
+
+using namespace std;
+
+int main()//0 N arasınd dizi içerisindeki n kadar syı üret üret 
+{
+	int N, n, i, j, Random;
+	cout << "Please enter the number pool amount you want to enter:";
+	cin >> N;
+	cout << "Please enter the number of numbersnumber of numbers:";
+	cin >> n;
+	cout << "Number pool amount:" << N << " " << "Number of selected numbers:" << n << endl;
+
+	int* number = new int[N];
+
+	for (i = 0; i < N; i++) {
+		cin >> number[i];
+	}
+	cout << "Entered numbers:";
+	for (j = 0; j < N; j++) {
+		cout << number[j] << " ";
+	}
+	cout << endl;
+
+
+	bool* y = new bool[N];
+	for (i = 0; i < N; i++) {
+		y[i] = false;
+	}
+
+	srand(time(NULL));
+	if (n > N)
+	{
+		for (i = 0; i < n; i++)
+		{
+			Random = rand() % N;
+			cout << number[Random] << " ";
+		}
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			while (y[Random = rand() % N] == true);
+			cout << number[Random] << " ";
+			y[Random] = true;
+		}
+	}
+}
+	
+
