3ae70eacf9d02ce43b8c0b7f0854192a2a0ef765
Arda Koyuncu <79996371+ardak555@users.noreply.github.com>
2022-03-22 19:23:15 +0300

    Add files via upload

---
+++ Simple Random Sempling.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int min, max, num, random, i, j, a, b ;
+
+    cout<<("Enter the minimum value:");
+	cin>>min;
+	cout << ("Enter the maximum value:");
+	cin >> max;
+	cout << ("How many numbers should be generated:");
+	cin >> num;
+	srand(time(NULL));
+
+	int* d = new int[num];
+	
+	if (num <= (max - min)) {
+		for (i = 0; i < num; i++)
+		{
+		e:
+			random = min + (rand() % (max - min));
+			for (j = 0; j < num; j++) {
+				if (random == d[j])
+					goto e;
+			}
+
+			d[i] = random;
+
+		}
+	}
+	else {
+		for (i = 0; i < num; i++)
+		{
+
+			random = min + (rand() % (max - min));
+			d[i] = random;
+
+		}
+	}
+
+	for (i = 0; i < num; i++)
+		cout<<d[i]<<" ";
+}
